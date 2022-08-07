51f12b7aca3a29aec5f618d2ac22cb2c2a3a5d39
Maksim Botalov <maxbotalov@icloud.com>
2022-08-07 23:48:19 +0500

    Initial Commit

---
+++ YoMarks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoMarks
+//
+//  Created by Максим Боталов on 07.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
