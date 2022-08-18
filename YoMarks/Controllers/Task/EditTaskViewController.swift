5b9149defbae45a66445ee59f0ce193a104c3d30
Maksim Botalov <dev.botalov@gmail.com>
2022-08-18 15:53:32 +0500

    Done

--- YoMarks/Controllers/Task/EditTaskViewController.swift
+++ YoMarks/Controllers/Task/EditTaskViewController.swift
@@ -13,6 +13,10 @@ class EditTaskViewController: UIViewController {
     // MARK: Property
     weak var delegate: UpdatingTaskProtocol?
     
+    private var contentViewSize: CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
     private var user: User
     private var task = TaskModel(title: "", description: "", id: "")
     
@@ -27,6 +31,21 @@ class EditTaskViewController: UIViewController {
     
     private let saveButton = UIButton(titleText: "Save", titleFont: FontSetup.medium(size: 16), titleColor: .white, backgroundColor: .black, isBorder: false, cornerRadius: 10, isShadow: true)
     private let deleteButton = UIButton(image: SystemImage.trash(), colorImage: .black, backgroundColor: ColorSetup.background(), isBorder: true, cornerRadius: 0, isShadow: true)
+    
+    private lazy var bgScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = ColorSetup.background()
+        scrollView.contentSize = contentViewSize
+        scrollView.frame = view.bounds
+        return scrollView
+    }()
+    
+    private lazy var conteinerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorSetup.background()
+        view.frame.size = contentViewSize
+        return view
+    }()
     
     // MARK: Custom Initializer
     init(user: User, task: TaskModel) {
@@ -47,6 +66,11 @@ class EditTaskViewController: UIViewController {
         
         saveButton.addTarget(self, action: #selector(saveEditTask), for: .touchUpInside)
         deleteButton.addTarget(self, action: #selector(deleteTask), for: .touchUpInside)
+        
+        registerNotificationKeyboard()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureAction))
+        conteinerView.addGestureRecognizer(tapGesture)
     }
     
     // MARK: viewDidLayoutSubviews
@@ -61,6 +85,10 @@ class EditTaskViewController: UIViewController {
     // MARK: Required initializer
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        removeNotificationKeyboard()
     }
 }
 
@@ -90,56 +118,85 @@ extension EditTaskViewController {
             }
         }
     }
+    
+    @objc private func tapGestureAction() {
+        titleTextField.textField.resignFirstResponder()
+        descriptionTextField.textField.resignFirstResponder()
+    }
+    
+    @objc private func keyboardShow(_ notification: Notification) {
+        bgScrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: -38, right: 0)
+    }
+    
+    @objc private func keyboardHide() {
+        bgScrollView.contentInset = UIEdgeInsets.zero
+    }
+}
+
+// MARK: - KeyboardWillShowNotification & KeyboardWillHideNotification
+extension EditTaskViewController {
+    private func registerNotificationKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func removeNotificationKeyboard() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 }
 
 // MARK: - Setting up constraints and auto layout
 extension EditTaskViewController {
     private func setupConstraints() {
-        view.addSubview(titleLabel)
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+        view.addSubview(bgScrollView)
+        bgScrollView.addSubview(conteinerView)
         
-        view.addSubview(titleTextField)
+        conteinerView.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: conteinerView.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            titleLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
+        
+        conteinerView.addSubview(titleTextField)
         NSLayoutConstraint.activate([
             titleTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
-            titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            titleTextField.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 16),
+            titleTextField.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -16),
             titleTextField.heightAnchor.constraint(equalToConstant: 50)])
         
-        view.addSubview(footTitleLabel)
+        conteinerView.addSubview(footTitleLabel)
         NSLayoutConstraint.activate([
             footTitleLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 4),
-            footTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            footTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+            footTitleLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            footTitleLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
         
-        view.addSubview(descriptionLabel)
+        conteinerView.addSubview(descriptionLabel)
         NSLayoutConstraint.activate([
             descriptionLabel.topAnchor.constraint(equalTo: footTitleLabel.bottomAnchor, constant: 20),
-            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+            descriptionLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            descriptionLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
         
-        view.addSubview(descriptionTextField)
+        conteinerView.addSubview(descriptionTextField)
         NSLayoutConstraint.activate([
             descriptionTextField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8),
-            descriptionTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            descriptionTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            descriptionTextField.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 16),
+            descriptionTextField.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -16),
             descriptionTextField.heightAnchor.constraint(equalToConstant: 50)])
         
-        view.addSubview(footDescriptionLabel)
+        conteinerView.addSubview(footDescriptionLabel)
         NSLayoutConstraint.activate([
             footDescriptionLabel.topAnchor.constraint(equalTo: descriptionTextField.bottomAnchor, constant: 4),
-            footDescriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            footDescriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+            footDescriptionLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            footDescriptionLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
 
         let buttonSV = UIStackView(arrangedSubviews: [saveButton, deleteButton], distribution: .fill, axis: .horizontal, spacing: 32)
         deleteButton.widthAnchor.constraint(equalToConstant: 48).isActive = true
-        view.addSubview(buttonSV)
+        conteinerView.addSubview(buttonSV)
         NSLayoutConstraint.activate([
             buttonSV.topAnchor.constraint(equalTo: footDescriptionLabel.bottomAnchor, constant: 40),
-            buttonSV.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            buttonSV.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            buttonSV.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            buttonSV.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32),
             buttonSV.heightAnchor.constraint(equalToConstant: 48)])
     }
 }
--- YoMarks/Controllers/Task/NewTaskViewController.swift
+++ YoMarks/Controllers/Task/NewTaskViewController.swift
@@ -11,6 +11,10 @@ class NewTaskViewController: UIViewController {
     
     weak var delegate: SaveTaskProtocol?
     
+    private var contentViewSize: CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
     // MARK: UI-components
     private let titleLabel = UILabel(text: "Heading", textColor: .black, font: FontSetup.bold(size: 17))
     private let descriptionLabel = UILabel(text: "Description", textColor: .black, font: FontSetup.bold(size: 17))
@@ -23,6 +27,21 @@ class NewTaskViewController: UIViewController {
     private let saveButton = UIButton(titleText: "Save", titleFont: FontSetup.medium(size: 16), titleColor: .white, backgroundColor: .black, isBorder: false, cornerRadius: 10, isShadow: true)
     private let closeButton = UIButton(image: SystemImage.close(), colorImage: .black, backgroundColor: ColorSetup.background(), isBorder: true, cornerRadius: 0, isShadow: true)
     
+    private lazy var bgScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = ColorSetup.background()
+        scrollView.contentSize = contentViewSize
+        scrollView.frame = view.bounds
+        return scrollView
+    }()
+    
+    private lazy var conteinerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorSetup.background()
+        view.frame.size = contentViewSize
+        return view
+    }()
+    
     // MARK: Lifecycle viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +51,11 @@ class NewTaskViewController: UIViewController {
         
         saveButton.addTarget(self, action: #selector(saveNewTask), for: .touchUpInside)
         closeButton.addTarget(self, action: #selector(closeViewController), for: .touchUpInside)
+        
+        registerNotificationKeyboard()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureAction))
+        conteinerView.addGestureRecognizer(tapGesture)
     }
     
     // MARK: Lifecycle viewWillAppear
@@ -48,6 +72,10 @@ class NewTaskViewController: UIViewController {
         saveButton.clipsToBounds = true
         closeButton.layer.cornerRadius = closeButton.frame.height / 2
         closeButton.clipsToBounds = true
+    }
+    
+    deinit {
+        removeNotificationKeyboard()
     }
 }
 
@@ -66,56 +94,85 @@ extension NewTaskViewController {
     @objc private func closeViewController() {
         dismiss(animated: true)
     }
+    
+    @objc private func tapGestureAction() {
+        titleTextField.textField.resignFirstResponder()
+        descriptionTextField.textField.resignFirstResponder()
+    }
+    
+    @objc private func keyboardShow(_ notification: Notification) {
+        bgScrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: -38, right: 0)
+    }
+    
+    @objc private func keyboardHide() {
+        bgScrollView.contentInset = UIEdgeInsets.zero
+    }
+}
+
+// MARK: - KeyboardWillShowNotification & KeyboardWillHideNotification
+extension NewTaskViewController {
+    private func registerNotificationKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func removeNotificationKeyboard() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 }
 
 // MARK: - Setting up constraints and auto layout
 extension NewTaskViewController {
     private func setupConstraints() {
-        view.addSubview(titleLabel)
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+        view.addSubview(bgScrollView)
+        bgScrollView.addSubview(conteinerView)
         
-        view.addSubview(titleTextField)
+        conteinerView.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: conteinerView.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            titleLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
+        
+        conteinerView.addSubview(titleTextField)
         NSLayoutConstraint.activate([
             titleTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
-            titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            titleTextField.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 16),
+            titleTextField.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -16),
             titleTextField.heightAnchor.constraint(equalToConstant: 50)])
         
-        view.addSubview(footTitleLabel)
+        conteinerView.addSubview(footTitleLabel)
         NSLayoutConstraint.activate([
             footTitleLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 4),
-            footTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            footTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+            footTitleLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            footTitleLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
         
-        view.addSubview(descriptionLabel)
+        conteinerView.addSubview(descriptionLabel)
         NSLayoutConstraint.activate([
             descriptionLabel.topAnchor.constraint(equalTo: footTitleLabel.bottomAnchor, constant: 20),
-            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+            descriptionLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            descriptionLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
         
-        view.addSubview(descriptionTextField)
+        conteinerView.addSubview(descriptionTextField)
         NSLayoutConstraint.activate([
             descriptionTextField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8),
-            descriptionTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            descriptionTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            descriptionTextField.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 16),
+            descriptionTextField.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -16),
             descriptionTextField.heightAnchor.constraint(equalToConstant: 50)])
         
-        view.addSubview(footDescriptionLabel)
+        conteinerView.addSubview(footDescriptionLabel)
         NSLayoutConstraint.activate([
             footDescriptionLabel.topAnchor.constraint(equalTo: descriptionTextField.bottomAnchor, constant: 4),
-            footDescriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            footDescriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)])
+            footDescriptionLabel.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            footDescriptionLabel.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32)])
 
         let buttonSV = UIStackView(arrangedSubviews: [saveButton, closeButton], distribution: .fill, axis: .horizontal, spacing: 32)
         closeButton.widthAnchor.constraint(equalToConstant: 48).isActive = true
-        view.addSubview(buttonSV)
+        conteinerView.addSubview(buttonSV)
         NSLayoutConstraint.activate([
             buttonSV.topAnchor.constraint(equalTo: footDescriptionLabel.bottomAnchor, constant: 40),
-            buttonSV.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            buttonSV.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            buttonSV.leadingAnchor.constraint(equalTo: conteinerView.leadingAnchor, constant: 32),
+            buttonSV.trailingAnchor.constraint(equalTo: conteinerView.trailingAnchor, constant: -32),
             buttonSV.heightAnchor.constraint(equalToConstant: 48)])
     }
 }
